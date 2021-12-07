ced2d8d2b1ad002e9e63d85e7fa4c2a8a89192e0
thomas <thomas.vandenberghe@gmail.com>
2021-12-07 15:28:57 +0100

    test heroku mardi

--- profil.php
+++ profil.php
@@ -1,12 +1,9 @@
 <?php
 session_start();
-$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_7caa586e0380adf;', 'b41a5dc6242a82' , 'df1cc1fa');
+$db = new PDO('mysql:host=eu-cdbr-west-01.cleardb.com;dbname=heroku_7caa586e0380adf','b41a5dc6242a82' , 'df1cc1fa');
 	// $db = new PDO('mysql:host=localhost;dbname=concoursChant', 'chant', '01021991');
 	//$db = new PDO('mysql:host=localhost:3307;dbname=concoursChant', 'chant', '01021991');
-	
-
 	?>
-    
    
 <html lang="en">
 <head>
@@ -91,12 +88,23 @@ if($_SESSION['pseudo']== "admin"){
 }
 ?>
 
-<h2> Etat d'avancement de votre inscription :</h2>
 
-</br>
+<?php 
+$dejaChoisi = $db->prepare("SELECT * FROM songs WHERE pseudo = ?");
+          $dejaChoisi->execute(array($_SESSION['pseudo']));
+          $fichierDB = $dejaChoisi->fetch();
+          $fichierCheck=$fichierDB['verif2'];
+          $fichierRejet=$fichierDB['rejeter2'];
 
+if($fichierCheck != "NON" ){ 
+?>
 
-		
+<p style="font-size: 29px; color:darkgreen; font-weight:bold">Votre inscription est quasiment terminée, il ne vous reste plus qu'à déposer votre chèque à la mairie de Longuenesse afin de valider votre participation.</p> 
+
+<?php
+}else{
+
+          ?>
 <?php if ($_SESSION['verif1'] != "NON" ){ ?>
     <?php
          $dejaChoisi = $db->prepare("SELECT * FROM songs WHERE pseudo = ?");
@@ -106,6 +114,15 @@ if($_SESSION['pseudo']== "admin"){
 echo"
 <p>Vous allez chanter la chanson: <b> $chansonCheck</b>. </p>";
   ?>
+
+<?php
+if($fichierRejet == "OUI"){
+    ?>
+    <p style=" color:red; font-size:19px;"><b>CETTE CHANSON A ETE REJETEE PAR L'ADMINISTRATEUR, VOUS POUVEZ FAIRE UNE NOUVELLE DEMANDE.</b>. </p>
+
+    <?php
+}
+?>
 
     <h2>Envoyez-nous votre bande son</h2>
 <div class ="formUpload">
@@ -151,6 +168,7 @@ echo"
 
     <?php } ?>
 
+
     <h2>Choix de votre chanson pour le concours.</h2>  
 <div class="submitContainer">
 <div class="containerFormChant">
@@ -173,7 +191,13 @@ echo"
 	</div>
 </div>
 
-    <?php } ?>
+    <?php
+
+}
+
+
+
+} ?>
 
 </div>
 
@@ -218,7 +242,7 @@ if (isset($_POST['submitsong']))
 
     $tmpName = $_FILES['uploaded_file']['tmp_name'];
     
-    $fileName = "upload/" . " choix de ". $_SESSION['pseudo'] . $fileExt;
+    $fileName = "upload/" . $_SESSION['pseudo'] . $fileExt;
     $resultat = move_uploaded_file($tmpName, $fileName);
 
     if ($resultat) 
@@ -240,16 +264,3 @@ if (isset($_POST['submitsong']))
 	})
 
 	</script>
-
-<!-- <script>
-	const validationChanson = document.querySelector("#formChanson");
-
-	validationChanson.addEventListener("submit",()=>{
-		if ( confirm("Vous allez soumettre votre demande de chanson")){
-            window.location.href="./profil.php"
-		}else{
-			window.location.href="./profil.php"
-		}
-	})
-
-	</script> -->
